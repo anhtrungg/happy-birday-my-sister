b0f1f959a7fa8fe2b8f411e16cf6e3d88cb8cc36
anhtrungg <anhtrung.work@gmail.com>
2022-11-08 19:35:45 +0700

    Updates

---
+++ precache-manifest.b0b703fa0770488c106d6f2fc625e77e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9593b2a5329b82d3f2da1132974ae61a",
+    "url": "/happy-birday-my-sister/index.html"
+  },
+  {
+    "revision": "7d8503394629fb046e69",
+    "url": "/happy-birday-my-sister/static/css/2.32048716.chunk.css"
+  },
+  {
+    "revision": "cca57122e2ee0e5873ff",
+    "url": "/happy-birday-my-sister/static/css/main.8533124d.chunk.css"
+  },
+  {
+    "revision": "7d8503394629fb046e69",
+    "url": "/happy-birday-my-sister/static/js/2.05bfb642.chunk.js"
+  },
+  {
+    "revision": "9fd8ac69f8779f68b807eb06f9423c10",
+    "url": "/happy-birday-my-sister/static/js/2.05bfb642.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cca57122e2ee0e5873ff",
+    "url": "/happy-birday-my-sister/static/js/main.2f90ea86.chunk.js"
+  },
+  {
+    "revision": "4f461ce8cd94fa4baeaf",
+    "url": "/happy-birday-my-sister/static/js/runtime-main.2be64d18.js"
+  },
+  {
+    "revision": "de28f71ec6eb8dfda2e68d2211ee49eb",
+    "url": "/happy-birday-my-sister/static/media/AvenirNextLTPro-Demi.de28f71e.otf"
+  }
+]);
