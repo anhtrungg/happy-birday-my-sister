c40e2f1721b333c2a4b6ff944ae4e8160b19663a
anhtrungg <anhtrung.work@gmail.com>
2022-11-08 21:48:55 +0700

    Ok

--- src/App.js
+++ src/App.js
@@ -35,10 +35,10 @@ const App = () => {
           <img src="https://firebasestorage.googleapis.com/v0/b/voice-project-f2a8b.appspot.com/o/phuong-anhn.jpg?alt=media&token=827ba218-98c6-4591-be85-aeaf9ba6c71f" alt="Phuong-Anh"></img>
         </div>
         <div className="img3">
-        <img className="img3" src="https://firebasestorage.googleapis.com/v0/b/voice-project-f2a8b.appspot.com/o/phuonganh-3.jpg?alt=media&token=b4cce413-c4c9-431c-b877-5bd16674956e" alt="Phuong-Anh"></img>
+        <img src="https://firebasestorage.googleapis.com/v0/b/voice-project-f2a8b.appspot.com/o/phuonganh-3.jpg?alt=media&token=b4cce413-c4c9-431c-b877-5bd16674956e" alt="Phuong-Anh"></img>
         </div>
         <div className="img2">
-        <img className="img2" src="https://firebasestorage.googleapis.com/v0/b/voice-project-f2a8b.appspot.com/o/phuonganh-2.jpg?alt=media&token=e1f0e3df-93d9-48a4-9af4-e007f0a0f4de" alt="Phuong-Anh"></img>
+        <img src="https://firebasestorage.googleapis.com/v0/b/voice-project-f2a8b.appspot.com/o/phuonganh-2.jpg?alt=media&token=e1f0e3df-93d9-48a4-9af4-e007f0a0f4de" alt="Phuong-Anh"></img>
         </div>
       </div >
     </div>
